f3c8198b839690531d9d1d56853bd61812b2110b
Urvashi-iPhone <lathiyaurvashi36@gmail.com>
2017-01-07 14:07:45 +0530

    Initial Commit

---
+++ Material Control/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Material Control
+//
+//  Created by Tecksky Techonologies on 1/7/17.
+//  Copyright © 2017 Tecksky Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
